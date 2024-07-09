be58c9a73948eb94765d69ee8a02380536aa36cd
axel gonzalez <ag180066@gmail.com>
2024-07-09 11:37:01 -0300

    ejercicio 8 terminado,cosa faltande

--- js/08-ejercicio.js
+++ js/08-ejercicio.js
@@ -2,12 +2,12 @@ const numero1 =parseInt(prompt("ingrese el primer numero "));
 
 if(numero1%2 == 0){
 
-document.write("el numero ingresado  "+numero1+ " es divisible en 2")
-
+document.write("el numero ingresado  "+numero1+ " es divisible en 2");
+console.log("el numero ingresado  "+numero1+ " es divisible en 2");
 }else{
 
-
+   
     document.write("el numero ingresado  "+numero1+ " no es divisible en 2")
-
+    console.log("el numero ingresado  "+numero1+ " no es divisible en 2");
 }
 
