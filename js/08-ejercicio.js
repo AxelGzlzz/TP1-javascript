9308b62f75ee166d2e01f1abf64f1cb295a04239
axel gonzalez <ag180066@gmail.com>
2024-07-09 11:18:31 -0300

    ejercicio 8 realizado

---
+++ js/08-ejercicio.js
@@ -0,0 +1,13 @@
+const numero1 =parseInt(prompt("ingrese el primer numero "));
+
+if(numero1%2 == 0){
+
+document.write("el numero ingresado  "+numero1+ " es divisible en 2")
+
+}else{
+
+
+    document.write("el numero ingresado  "+numero1+ " no es divisible en 2")
+
+}
+
