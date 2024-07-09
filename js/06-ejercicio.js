204b75ca6d9c416a6243744bcbcf9f448134cad2
axel gonzalez <ag180066@gmail.com>
2024-07-09 10:57:57 -0300

    ejercicio 6 realizado

---
+++ js/06-ejercicio.js
@@ -0,0 +1,14 @@
+const numero1 =parseInt(prompt("ingrese el primer numero "));
+const numero2 =parseInt(prompt("ingrese el segundo numero "));
+
+
+if(numero1>numero2){
+
+document.write("El numero mas grande es el : "+numero1);
+console.log("El numero mas grande es el : "+numero1);
+
+
+}else{
+    document.write("El numero mas grande es el : "+numero2);
+    console.log("El numero mas grande es el : "+numero2);
+}
