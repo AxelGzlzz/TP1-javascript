015b8cede6c2f1a6aa5441eaefe1fc3cd32ff616
axel gonzalez <ag180066@gmail.com>
2024-07-09 10:44:29 -0300

    ejercicio 5 realizado

---
+++ js/05-ejercicio.js
@@ -0,0 +1,8 @@
+const numero1 =parseInt(prompt("ingrese el primer numero "));
+const numero2 =parseInt(prompt("ingrese el segundo numero "));
+
+const suma =numero1+numero2;
+
+document.write("el resultado de la suma es : "+ suma )
+
+
